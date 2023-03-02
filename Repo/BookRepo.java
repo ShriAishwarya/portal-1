dcce0a58c07be6f7c6a30ca3e631159fc94ad2d1
ShriAishwarya <113581480+ShriAishwarya@users.noreply.github.com>
2023-03-02 15:47:30 +0530

    Delete BookRepo.java

--- Repo/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Models.Book;
-
-public interface BookRepo extends JpaRepository<Book,Integer>{
-
-}
